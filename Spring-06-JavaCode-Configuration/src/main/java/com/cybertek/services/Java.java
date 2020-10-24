9af6bec59b4233a93fee1d061510557b0853c1d2
Can-Ali <can.ali73@gmail.com>
2020-10-24 19:15:28 +0100

    Spring Configuration Annotation

---
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,20 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Java implements Course {
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours : 30");
+    }
+
+}
