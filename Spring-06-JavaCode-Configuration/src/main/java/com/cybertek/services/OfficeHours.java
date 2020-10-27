250a605a2458e56e8094217c903e9e968200a387
Can-Ali <can.ali73@gmail.com>
2020-10-27 23:37:07 +0000

    Spring Post constructor and pre destroy

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/OfficeHours.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/OfficeHours.java
@@ -3,12 +3,10 @@ package com.cybertek.services;
 import com.cybertek.interfaces.ExtraSessions;
 import org.springframework.stereotype.Component;
 
-@Component
-public class OfficeHours implements ExtraSessions {
 
+public class OfficeHours implements ExtraSessions {
     @Override
     public int getHours() {
-       return 4;
+        return 7;
     }
-
 }
--- Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Selenium.java
@@ -3,9 +3,10 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 import org.springframework.stereotype.Component;
 
+@Component
 public class Selenium implements Course {
 
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours 22");
+        System.out.println("Teaching hours : 20");
     }
 }
