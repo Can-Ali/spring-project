cb1e9e2445f9b90199f220c719c160bb370ae5fa
Can-Ali <can.ali73@gmail.com>
2020-12-19 18:31:30 +0000

    data.sql added

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/CinemaLabApplication.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/CinemaLabApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CybertekApplication {
+public class CinemaLabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CybertekApplication.class, args);
+		SpringApplication.run(CinemaLabApplication.class, args);
 	}
 
 }
