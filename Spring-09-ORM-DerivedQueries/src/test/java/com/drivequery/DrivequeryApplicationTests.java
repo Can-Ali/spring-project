4e71ed0e59e6d6c7b130142c92f8fea247b815cb
Can-Ali <can.ali73@gmail.com>
2020-12-25 15:41:47 +0000

    Spring ORM Derived Queries

---
+++ Spring-09-ORM-DerivedQueries/src/test/java/com/drivequery/DrivequeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivequery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivequeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
