1cffef306584ecf240418b3c475d8d3e3c88896d
Can-Ali <can.ali73@gmail.com>
2021-03-27 09:04:17 +0000

    Spring-modications singleton and prototype scopes

--- Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Selenium.java
@@ -1,9 +1,17 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import lombok.Data;
 
+//@Getter
+//@Setter
+//@NoArgsConstructor
+@Data
 public class Selenium implements Course {
+
+    private OfficeHours officeHours;
+
     public void getTeachingHours() {
-        System.out.println("Weekly Teaching Hours :  15");
+        System.out.println("Weekly Teaching Hours :  15" + " + " + officeHours.getHours());
     }
 }
