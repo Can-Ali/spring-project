dc600e0ea32f99e74b6733aa7cef0858e4aa3138
Can-Ali <can.ali73@gmail.com>
2021-04-03 16:41:26 +0100

    Spring-09 ORM modications

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/OrmConfigApplication.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/OrmConfigApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class OrmConfigApplication {
@@ -22,13 +24,24 @@ public class OrmConfigApplication {
 	@PostConstruct
 	public void dataInit(){
 
+
+
 		Car c1 = new Car("BMW","M5");
 		Car c2 = new Car("Kia","Sorento");
 		Car c3 = new Car("Mercedes","S500");
+		Car c4 = new Car("Volvo","XC60");
 
-		carRepository.save(c1);
-		carRepository.save(c2);
-		carRepository.save(c3);
+		List<Car> cars = new ArrayList<>();
+		cars.add(c1);
+		cars.add(c2);
+		cars.add(c3);
+		cars.add(c4);
+
+		carRepository.saveAll(cars);
+
+//		carRepository.save(c1);
+//		carRepository.save(c2);
+//		carRepository.save(c3);
 
 	}
 }
